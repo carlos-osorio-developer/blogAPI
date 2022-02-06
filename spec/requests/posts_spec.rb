b275c2bb577d7d05174a9874dcb613a71a718ca2
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-06 15:55:22 -0500

    Add routes and fix routes in tests for posts requests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /post" do
-    before { get "/post" }
+  describe "GET /posts" do    
 
     it "returns OK" do
+      get "/posts"
       payload = JSON.parse(response.body)
       expect(payload).to be_empty
       expect(response).to have_http_status(200) 
@@ -15,18 +15,18 @@ RSpec.describe "Posts", type: :request do
     let(:posts) { create_list(:post, 10, published: true) }
 
     it "returns all posts" do
-      get "/post"
+      get "/posts"
       payload = JSON.parse(response.body)
       expect(payload.size).to eq(10)
       expect(response).to have_http_status(200)
     end
   end
      
-  describe "GET /post/:id" do
+  describe "GET /posts/:id" do
     let(:post) { create(:post, published: true) }
 
     it "returns a post" do
-      get "/post/#{post.id}"
+      get "/posts/#{post.id}"
       payload = JSON.parse(response.body)
       expect(payload).not_to be_empty
       expect(payload['id']).to eq(post.id)
