47ab6adce9aad1f35eae997e4eaa56540b3bfc15
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-06 15:04:14 -0500

    Add validations and relations for user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :auth_token, presence: true, uniqueness: true
+
+  has_many :posts, dependent: :destroy
 end
