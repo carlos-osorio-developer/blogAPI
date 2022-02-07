da1237b3e8abbf6d536dca60f3cfc68217536fb3
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-06 21:07:54 -0500

    Add controller methods to create and update a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render json: @post, status: :ok
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post, status: :ok
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
 end
