af5622c97769495f45b3c1141142f0f657a4e225
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-07 15:18:09 -0500

    Use the mailer from the background job

--- app/jobs/post_report_job.rb
+++ app/jobs/post_report_job.rb
@@ -6,5 +6,8 @@ class PostReportJob < ApplicationJob
     post = Post.find(post_id)
 
     report = PostReport.generate(post)
+
+    PostReportMailer.post_report(user, post, report).deliver_now 
+    #could use deliver_later but we are already in a background job
   end
 end
